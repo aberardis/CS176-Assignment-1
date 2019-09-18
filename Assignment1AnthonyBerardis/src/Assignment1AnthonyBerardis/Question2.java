eab1a053d2fe5b20b07fa9de63dd3f95b2e55bc0
aberardis <54951442+aberardis@users.noreply.github.com>
2019-09-18 11:35:02 -0400

    Update Question2.java

--- Assignment1AnthonyBerardis/src/Assignment1AnthonyBerardis/Question2.java
+++ Assignment1AnthonyBerardis/src/Assignment1AnthonyBerardis/Question2.java
@@ -1,10 +1,30 @@
 package Assignment1AnthonyBerardis;
 
+import java.util.Scanner;
+
 public class Question2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the element");
+
+		int array[] = new int[5];
+		int total = 0;
+		int a = 0;
+
+		System.out.println();
+
+		for (int i = 0; i <= 4; i++) {
+
+			System.out.print(i);
+
+			array[a] = input.nextInt();
+			a++;
+
+		}
+		for (int num : array)
+			total += num;
+		System.out.print(total);
 	}
-
 }
