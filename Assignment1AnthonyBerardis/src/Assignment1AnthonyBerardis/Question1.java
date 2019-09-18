2041acc13dae5aad9a592585c8996d476f5e4795
aberardis <54951442+aberardis@users.noreply.github.com>
2019-09-18 09:55:41 -0400

    Code

---
+++ Assignment1AnthonyBerardis/src/Assignment1AnthonyBerardis/Question1.java
@@ -0,0 +1,21 @@
+package Assignment1AnthonyBerardis;
+
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int intArray[];  //declaring array
+		intArray = new int[10]; //creates spaces for array
+		
+		//initializes first element
+		intArray[0]=17; 
+		
+		//initializes last element
+		intArray[9]=29; 
+		
+	System.out.println(intArray[9]);	
+
+	}
+
+}
